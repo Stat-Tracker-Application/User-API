3da3216e74bb6e9322a855df8e3c79ab8c5addca
Hagoort, Bart <42860246+LokiTheShadySpirit@users.noreply.github.com>
2023-10-27 08:45:50 +0200

    More or less fixed htpps

--- Api/Server.js
+++ Api/Server.js
@@ -5,9 +5,9 @@ import app from "./API.js";
 const port = process.env.PORT || 5000;
 
 const options = {
-  ca: fs.readFileSync("./Certificates/stattrackerapp.test.csr"),
-  key: fs.readFileSync("./Certificates/stattrackerapp.test.key"),
-  cert: fs.readFileSync("./Certificates/stattrackerapp.test.crt"),
+  ca: fs.readFileSync("../Certificates/ca.crt"),
+  key: fs.readFileSync("./Certs/localhost.key"),
+  cert: fs.readFileSync("./Certs/localhost.crt"),
 };
 const server = https.createServer(options, app);
 
