3766f137ab4e09093d173ace11bb5804e6c11b46
Hagoort, Bart <42860246+LokiTheShadySpirit@users.noreply.github.com>
2023-11-16 09:47:56 +0100

    intermittant push

--- Api/Server.js
+++ Api/Server.js
@@ -1,15 +1,20 @@
 import https from "https";
+import http from "http";
 import fs from "fs";
 import app from "./API.js";
 
 const port = process.env.PORT || 5000;
 
 const options = {
-  ca: fs.readFileSync("../Certificates/ca.crt"),
+  requestCert: true,
+  rejectUnauthorized: false,
+  ca: fs.readFileSync("./Certs/ca.crt"),
   key: fs.readFileSync("./Certs/localhost.key"),
   cert: fs.readFileSync("./Certs/localhost.crt"),
 };
-const server = https.createServer(options, app);
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; //dirty fix as discussed
+const server = http.createServer(options, app);
 
 server.listen(port, function (req, res) {
   console.log(`Server is listening at port ${port}`);
